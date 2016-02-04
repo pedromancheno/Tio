18a7cf149340d9d3c56604786e30ab8f0f0d2fb7
Pedro Mancheno <pedromancheno@gmail.com>
2016-02-04 23:24:18 +0100

    Create Medium model

---
+++ Tio/Source/Medium.swift
@@ -0,0 +1,117 @@
+//
+//  Medium.swift
+//  Tio
+//
+//  Created by Pedro Mancheno on 04/02/16.
+//  Copyright © 2016 Tio. All rights reserved.
+//
+
+import Foundation
+
+enum Status {
+    case Released
+    case Postroduction
+}
+
+class Medium {
+
+    let adult: Bool
+    let budget: Double
+    let genres: Array<Genres>
+    let homepage: String
+    let id: UInt
+    let imdbId: UInt
+    let originalLanguage: String
+    let originalTitle: String
+    let overview: String
+    let popularity: Double
+    let posterPath: String
+    let productionCompanies: Array<ProductionCompany>
+    let productionCountries: Array<Country>
+    let releaseDateTimestamp: String
+    let revenue: UInt
+    let runtime: UInt
+    let spokenLanguages: Array<Language>
+    let status: String
+    let tagLine: String
+    let title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: UInt
+    
+    
+    init(
+        adult: Bool,
+        budget: Double,
+        genres: Array<Genres>,
+        homepage: String,
+        id: UInt,
+        imdbId: UInt,
+        originalLanguage: String,
+        originalTitle: String,
+        overview: String,
+        popularity: Double,
+        posterPath: String,
+        productionCompanies: Array<ProductionCompany>,
+        productionCountries: Array<Country>,
+        releaseDateTimestamp: String,
+        revenue: UInt,
+        runtime: UInt,
+        spokenLanguages: Array<Language>,
+        status: String,
+        tagLine: String,
+        title: String,
+        video: Bool,
+        voteAverage: Double,
+        voteCount: UInt
+        ) {
+   
+           self.adult = adult
+           self.budget = budget
+           self.genres = genres
+           self.homepage = homepage
+           self.id = id
+           self.imdbId = imdbId
+           self.originalLanguage = originalLanguage
+           self.originalTitle = originalLanguage
+           self.overview = overview
+           self.popularity = popularity
+           self.posterPath = posterPath
+           self.productionCompanies = productionCompanies
+           self.productionCountries = productionCountries
+           self.releaseDateTimestamp = releaseDateTimestamp
+           self.revenue = revenue
+           self.runtime = runtime
+           self.spokenLanguages = spokenLanguages
+           self.status = status
+           self.tagLine = tagLine
+           self.title = title
+           self.video = video
+           self.voteAverage = voteAverage
+           self.voteCount = voteCount
+    }
+}
+
+struct Genres {
+    
+    let id: UInt
+    let name: String
+}
+
+struct ProductionCompany {
+    
+    let name: String
+    let id: UInt
+}
+
+struct Country {
+    
+    let name: String
+    let iso31661: String
+}
+
+struct Language {
+    
+    let name: String
+    let iso6391: String
+}
