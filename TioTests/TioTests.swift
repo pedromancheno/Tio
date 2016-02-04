d38b231d9b29297c9fc9d7c130d929b29fbcc49d
Pedro Mancheno <pedromancheno@gmail.com>
2016-02-04 22:24:34 +0100

    Initial Commit

---
+++ TioTests/TioTests.swift
@@ -0,0 +1,36 @@
+//
+//  TioTests.swift
+//  TioTests
+//
+//  Created by Pedro Mancheno on 04/02/16.
+//  Copyright © 2016 Tio. All rights reserved.
+//
+
+import XCTest
+@testable import Tio
+
+class TioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
